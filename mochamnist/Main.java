ef9689a77c879e5adb4a83dea390ff707712a357
kvnx <35074883+kvnx@users.noreply.github.com>
2022-09-12 21:55:39 -0500

    setup to use mocha jar relative to this directory

---
+++ mochamnist/Main.java
@@ -0,0 +1,13 @@
+package mochamnist;
+
+import io.github.kevin.mocha.Neuron;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Neuron test = new Neuron();
+        
+    }
+
+}
