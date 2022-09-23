f80df78d5693ae5474894dbf0d904e30d2186460
kvnx <35074883+kvnx@users.noreply.github.com>
2022-09-23 10:39:37 -0500

    basic neural network for testing

--- mochamnist/Main.java
+++ mochamnist/Main.java
@@ -1,12 +1,41 @@
 package mochamnist;
 
-import io.github.kevin.mocha.Neuron;
+import io.github.kevin.mocha.NeuralNetwork;
 
 public class Main {
     
     public static void main(String[] args) {
         
-        Neuron test = new Neuron();
+        NeuralNetwork nn = new NeuralNetwork(new int[] {784, 16, 16, 10});
+//        System.out.println(nn);
+//        System.out.println(nn.getNeuron(2, 14).getOut(9) == nn.getNeuron(3, 9).getIn(14));
+        
+        NeuralNetwork simple = new NeuralNetwork(new int[] {3, 1});
+        
+        float[][] trainingData = new float[10][3];
+        float[][] expectedData = new float[10][1];
+        // generate random cases for this
+        for (int i = 0; i < trainingData.length; i++) {
+            for (int j = 0; j < trainingData[i].length; j++) {
+                trainingData[i][j] = Math.random() < 0.5 ? 0 : 1;
+            }
+            expectedData[i][0] = trainingData[i][0];
+        }
+        
+        simple.train(trainingData, expectedData);
+        
+        float[][] testData = new float[10][3];
+        // generate random cases for this
+        for (int i = 0; i < testData.length; i++) {
+            for (int j = 0; j < testData[i].length; j++) {
+                testData[i][j] = Math.random() < 0.5 ? 0 : 1;
+            }
+            expectedData[i][0] = testData[i][0];
+        }
+        
+        for (int i = 0; i < testData.length; i++) {
+            System.out.println(simple.predict(testData[i])[0] + " Actual: " + expectedData[i][0]);
+        }
         
     }
 
